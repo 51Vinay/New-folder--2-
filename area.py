99f3c4170fbbd2ee919b8631c32427ebac10680f
vinay sharma <vinaysharma5335@gmail.com>
2022-03-27 14:45:08 +0530

    this is area program

---
+++ area.py
@@ -0,0 +1,15 @@
+l=int(input("Enter the first value :-"))
+b=int(input("Enter the first value :-"))
+r=int(input("Enter the first value :-"))
+area=l*b
+are=2*l+2*b
+squa=l*l
+sq=4*l
+cir=3.14*r*r
+circum=2*3.14*r
+print("This is area of rectangle :-",area)
+print("This is Perimeter of rectangle :-",are)
+print("This is area of square :-",squa)
+print("This is perimeter of square :-",sq)
+print("This is area of Circle :-",cir)
+print("This is circumference of Circle :-",circum)
